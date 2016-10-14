48e0fb119e852931f20302c4fe9de11fe567e40b
Nick Carneiro <nickc@trillworks.com>
2016-10-14 13:13:25 -0700

    added some standard-related files for webstorn ide

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
     standard: {
       app: {
         src: [
-            'index.js', 'generators/*.js'
+          'index.js', 'generators/*.js', 'test.js'
         ]
       }
     },
