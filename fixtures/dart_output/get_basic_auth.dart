f329c00177ce0b9d3a94581fac39b4d6c8214591
csells <csells@sellsbrothers.com>
2019-07-25 21:29:55 -0700

    Dart support for charles syntax and a little refactoring

--- fixtures/dart_output/get_basic_auth.dart
+++ fixtures/dart_output/get_basic_auth.dart
@@ -2,11 +2,10 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 void main() async {
-  var headers = Map<String, String>();
   var uname = 'some_username';
   var pword = 'some_password';
-  headers['Authentication'] = 'Basic ' + convert.base64Encode(convert.utf8.encode('$uname:$pword'));
+  var authn = 'Basic ' + convert.base64Encode(convert.utf8.encode('$uname:$pword'));
 
-  var res = await http.get('https://api.test.com/', headers: headers);
+  var res = await http.get('https://api.test.com/', headers: {'Authentication': authn});
   if (res.statusCode != 200) throw Exception('get error: statusCode= ${res.statusCode}');
 }
--- generators/dart.js
+++ generators/dart.js
@@ -5,33 +5,39 @@ var toDart = function (curlCommand) {
   var r = util.parseCurlCommand(curlCommand)
   var s = "";
 
+  if (r.auth) s += "import 'dart:convert' as convert;\n";
+
   s +=
     "import 'package:http/http.dart' as http;\n" +
     "\n" +
     "void main() async {\n";
 
-  var hasHeaders = r.auth;
-  if (hasHeaders) s += "  var headers = Map<String, String>();\n";
+  if (r.auth) {
+    var splitAuth = r.auth.split(':');
+    var uname = splitAuth[0] || '';
+    var pword = splitAuth[1] || '';
 
-  // if (request.headers || request.cookies) {
-  //   dartCode += 'var headers = {\n'
-  //   var headerCount = Object.keys(request.headers).length
-  //   var i = 0
-  //   for (var headerName in request.headers) {
-  //     dartCode += '    \'' + headerName + '\': \'' + request.headers[headerName] + '\''
-  //     if (i < headerCount - 1 || request.cookies) {
-  //       dartCode += ',\n'
-  //     } else {
-  //       dartCode += '\n'
-  //     }
-  //     i++
-  //   }
-  //   if (request.cookies) {
-  //     var cookieString = util.serializeCookies(request.cookies)
-  //     dartCode += '    \'Cookie\': \'' + cookieString + '\'\n'
-  //   }
-  //   dartCode += '};\n\n'
-  // }
+    s +=
+      "  var uname = '" + uname + "';\n" +
+      "  var pword = '" + pword + "';\n" +
+      "  var authn = 'Basic ' + convert.base64Encode(convert.utf8.encode('$uname:$pword'));\n" +
+      "\n";
+  }
+
+  var hasHeaders = r.headers || r.cookies;
+  if (hasHeaders) {
+    s += "  var headers = {\n";
+    for (var hname in r.headers) s += "    '" + hname + "': '" + r.headers[hname] + "',\n";
+
+    if (r.cookies) {
+      var cookiestr = util.serializeCookies(r.cookies)
+      s += "    'Cookie': '" + cookiestr + "'\n";
+    }
+
+    if (r.auth) s += "    'Authentication': authn,\n";
+    s += "  };\n";
+    s += "\n";
+  }
 
   // if (request.data) {
   //   // escape single quotes if there are any in there
@@ -47,30 +53,14 @@ var toDart = function (curlCommand) {
   //   dartCode += ',\n    method: \'' + request.method.toUpperCase() + '\''
   // }
 
-  // if (request.headers || request.cookies) {
-  //   dartCode += ',\n'
-  //   dartCode += '    headers: headers'
-  // }
   // if (request.data) {
   //   dartCode += ',\n    body: dataString'
   // }
 
-  if (r.auth) {
-    s = "import 'dart:convert' as convert;\n" + s;
-
-    var splitAuth = r.auth.split(':');
-    var uname = splitAuth[0] || '';
-    var pword = splitAuth[1] || '';
-
-    s +=
-      "  var uname = '" + uname + "';\n" +
-      "  var pword = '" + pword + "';\n" +
-      "  headers['Authentication'] = 'Basic ' + convert.base64Encode(convert.utf8.encode('$uname:$pword'));\n" +
-      "\n";
-  }
 
   s += "  var res = await http." + r.method + "('" + r.url + "'"
   if (hasHeaders) s += ", headers: headers";
+  else if (r.auth) s += ", headers: {'Authentication': authn}";
 
   s +=
     ");\n" +
