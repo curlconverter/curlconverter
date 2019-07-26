f686d40c6478e960f5ddf208e6c66bb4ad736a2f
csells <csells@sellsbrothers.com>
2019-07-25 20:16:44 -0700

    Dart more basic auth testings

---
+++ fixtures/dart_output/get_basic_auth.dart
@@ -0,0 +1,12 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+void main() async {
+  var headers = Map<String, String>();
+  var uname = 'some_username';
+  var pword = 'some_password';
+  headers['Authentication'] = 'Basic ' + convert.base64Encode(convert.utf8.encode('$uname:$pword'));
+
+  var res = await http.get('https://api.test.com/', headers: headers);
+  if (res.statusCode != 200) throw Exception('get error: statusCode= ${res.statusCode}');
+}
