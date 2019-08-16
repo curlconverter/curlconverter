ee17362705600002f3c1703d09edb43389282eb1
Connor F <connorfinnigan@gmail.com>
2019-08-16 11:53:04 -0500

    new test curl and test python output for cookie in lowercase

---
+++ fixtures/python_output/get_with_header_lowercase_cookie.py
@@ -0,0 +1,14 @@
+import requests
+
+cookies = {
+    'secure_user_id': 'InNlY3VyZTEwNjI3Ig%3D%3D--3b5df49345735791f2b80eddafb630cdcba76a1d',
+    'adaptive_image': '1440',
+    'has_js': '1',
+    'ccShowCookieIcon': 'no',
+    '_web_session': 'Y2h...e5',
+}
+
+headers = {
+}
+
+response = requests.get('http://www.url.com/page', headers=headers, cookies=cookies)
