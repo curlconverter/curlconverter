3f8daf42091ff1a3a9efd0033711c5ecf4f52b12
Mohammed Ramli <xc0d3rz@users.noreply.github.com>
2019-11-23 18:17:31 +0200

    Fix typo error

--- generators/custom.js
+++ generators/custom.js
@@ -2,7 +2,7 @@ var util = require('../util')
 var jsesc = require('jsesc')
 
 var custom = function (curlCommand, cb) {
-  if (cb && typeof cb === 'function') cb(util.parseCurlCommand(curlCommand, util, jsesc))
+  if (cb && typeof cb === 'function') cb(util.parseCurlCommand(curlCommand), util, jsesc)
 }
 
 module.exports = custom
