19c6cf2c6f1940648e60dcd516b60bd8e47df536
Dainis Gorbunovs <dgdev@protonmail.com>
2020-03-12 02:03:16 +0000

    Fix indentation issue & missing brace [MATLAB]

---
+++ fixtures/matlab_output/get_cookie_with_empty_value.m
@@ -0,0 +1,28 @@
+%% Web Access using Data Import and Export API
+cookies = {
+    'mysamplecookie' 'someValue'
+    'emptycookie' ''
+    'otherCookie' '2'
+};
+uri = 'https://httpbin.org/cookies';
+options = weboptions('HeaderFields', {
+    'accept' 'application/json'
+    'Cookie' char(join(join(cookies, '='), '; '))
+});
+response = webread(uri, options);
+
+%% HTTP Interface
+import matlab.net.*
+import matlab.net.http.*
+
+cookies = {
+    'mysamplecookie' 'someValue'
+    'emptycookie' ''
+    'otherCookie' '2'
+};
+header = [
+    HeaderField('accept', 'application/json')
+    field.CookieField(cellfun(@(x) Cookie(x{:}), num2cell(cookies, 2)))
+]';
+uri = URI('https://httpbin.org/cookies');
+response = RequestMessage('get', header).send(uri.EncodedURI);
