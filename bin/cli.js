b4d290827476de8a6ba27f8d54a8b9923f23bfd1
Борис Верховский <boris.verk@gmail.com>
2021-09-23 13:38:51 -0600

    make cli act as a drop-in replacement for curl

--- bin/cli.js
+++ bin/cli.js
@@ -2,12 +2,7 @@
 
 import * as curlconverter from '../index.js'
 
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 import fs from 'fs'
-
-// sets a default in case -l/--langauge isn't passed
-const defaultLanguage = 'python'
 
 // used to map languages to functions
 // NOTE: make sure to update this when adding language support
@@ -17,38 +12,56 @@ const translate = {
   dart: 'toDart',
   elixir: 'toElixir',
   go: 'toGo',
+  java: 'toJava',
   json: 'toJsonString',
+  matlab: 'toMATLAB',
   node: 'toNodeFetch',
   'node-request': 'toNodeRequest',
   php: 'toPhp',
   python: 'toPython',
   r: 'toR',
   rust: 'toRust',
-  strest: 'toStrest',
-  matlab: 'toMATLAB',
-  java: 'toJava'
+  strest: 'toStrest'
 }
 
-const argv = yargs(hideBin(process.argv))
-  .scriptName('curlconverter')
-  .usage('Usage: $0 [-l <language>] [curl_command]')
-  .option('l', {
-    alias: 'language',
-    choices: Object.keys(translate),
-    demandOption: false,
-    default: defaultLanguage,
-    describe: 'the language to convert the curl command to',
-    type: 'string'
-  })
-  .positional('curl_command', {
-    describe: 'if no <curl_command> is passed, the script will read from stdin',
-    type: 'string'
-  })
-  .alias('h', 'help')
-  .help()
-  .argv
+const USAGE = `Usage: curlconverter [<language>] [curl_options...]
 
-const curl = argv._.length ? argv._[0] : fs.readFileSync(0, 'utf8')
-const generator = curlconverter[translate[argv.language]]
+language: the language to convert the curl command to. The choices are
+  ansible
+  browser
+  dart
+  elixir
+  go
+  java
+  json
+  matlab
+  node
+  node-request
+  php
+  python (the default)
+  r
+  rust
+  strest
+
+If no <curl_options> are passed, the script will read from stdin.`
+
+let language = 'python'
+let argv = process.argv.slice(2)
+if (argv.includes('--help') || argv.includes('-h')) {
+  console.log(USAGE.trim())
+  process.exit(0)
+}
+if (Object.prototype.hasOwnProperty.call(translate, argv[0])) {
+  [language, ...argv] = argv
+}
+
+// Warning for users using the pre-4.0 CLI
+if (argv.length === 1 && argv[0].startsWith('curl ')) {
+  console.error('warning: Passing a whole curl command as a single argument?')
+  console.error('warning: Pass options to curlconverter as if it was curl instead:')
+  console.error("warning: curlconverter 'curl example.com' -> curlconverter example.com")
+}
+const curl = argv.length ? ['curl', ...argv] : fs.readFileSync(0, 'utf8')
+const generator = curlconverter[translate[language]]
 const code = generator(curl)
 process.stdout.write(code)
