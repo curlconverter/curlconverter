f1e17cb3ddbdeaa9c414d3cbb705ffd077fc2811
Boris Verkhovskiy <boris.verk@gmail.com>
2022-03-14 18:11:55 -0700

    Rename "browser" to "javascript" (#338)

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,14 @@
 import { curlLongOpts, curlShortOpts, parseArgs, buildRequest, parseCurlCommand, CCError } from '../util.js'
 
 import { _toAnsible } from '../generators/ansible.js'
-import { _toBrowser } from '../generators/javascript/browser.js'
 import { _toDart } from '../generators/dart.js'
 import { _toElixir } from '../generators/elixir.js'
 import { _toGo } from '../generators/go.js'
 import { _toJava } from '../generators/java.js'
+import { _toJavaScript } from '../generators/javascript/javascript.js'
 import { _toJsonString } from '../generators/json.js'
 import { _toMATLAB } from '../generators/matlab/matlab.js'
-import { _toNodeFetch } from '../generators/javascript/node-fetch.js'
+import { _toNode } from '../generators/javascript/node-fetch.js'
 import { _toNodeRequest } from '../generators/javascript/node-request.js'
 import { _toPhp } from '../generators/php.js'
 import { _toPython } from '../generators/python.js'
@@ -30,14 +30,15 @@ const defaultLanguage = 'python'
 // NOTE: make sure to update this when adding language support
 const translate = {
   ansible: _toAnsible,
-  browser: _toBrowser,
+  browser: _toJavaScript, // backwards compatibility, undocumented
   dart: _toDart,
   elixir: _toElixir,
   go: _toGo,
   java: _toJava,
+  javascript: _toJavaScript,
   json: _toJsonString,
   matlab: _toMATLAB,
-  node: _toNodeFetch,
+  node: _toNode,
   'node-request': _toNodeRequest,
   php: _toPhp,
   python: _toPython,
@@ -50,11 +51,11 @@ const USAGE = `Usage: curlconverter [--language <language>] [curl_options...]
 
 language: the language to convert the curl command to. The choices are
   ansible
-  browser
   dart
   elixir
   go
   java
+  javascript
   json
   matlab
   node
