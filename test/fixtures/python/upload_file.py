e2beef7188dfb62af5172ad4ebdb9f78918547a2
Борис Верховский <boris.verk@gmail.com>
2022-04-15 13:02:43 -0700

    update test

--- test/fixtures/python/upload_file.py
+++ test/fixtures/python/upload_file.py
@@ -3,4 +3,4 @@ import requests
 with open('file.txt', 'rb') as f:
     data = f.read()
 
-response = requests.put('http://localhost:28139', data=data)
+response = requests.put('http://localhost:28139/file.txt', data=data)
