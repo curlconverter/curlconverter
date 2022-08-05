ffe96ad447ed6b4c861282390ff9771ee83eef28
Boris Verkhovskiy <boris.verk@gmail.com>
2022-08-05 13:49:47 -0700

    Always load tree-sitter-bash.wasm from top level
    for consistency with how tree-sitter loads tree-sitter.wasm

--- src/bash-parser-web.ts
+++ src/bash-parser-web.ts
@@ -3,7 +3,7 @@ import Parser from "web-tree-sitter";
 // NOTE: Top-level await is not available in Safari until Safari 15
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await#browser_compatibility
 await Parser.init();
-const Bash = await Parser.Language.load("tree-sitter-bash.wasm");
+const Bash = await Parser.Language.load("/tree-sitter-bash.wasm");
 const parser = new Parser();
 parser.setLanguage(Bash);
 
