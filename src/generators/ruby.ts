31d5be5a2d4151f310f6e239a32d96890cc4657c
Boris Verkhovskiy <boris.verk@gmail.com>
2022-08-14 11:13:42 -0700

    remove license
    there's nothing that justifies it

--- src/generators/ruby.ts
+++ src/generators/ruby.ts
@@ -1,29 +1,3 @@
-// This code is adapted from curl-to-ruby, which is a fork of curl-to-go,
-// which is licensed under the MIT license.
-// https://github.com/jhawthorn/curl-to-ruby/blob/24abdb538dd269161ee9ad6c2658e7e37d37332e/src/curlToRuby.js
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 Matthew Holt
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 import * as util from "../util.js";
 import type { Request, Warnings } from "../util.js";
 
