5d3e8899d6bde664cd05df6532c5bd8ba064519a
Cömert Baldemir <comertbaldemir@gmail.com>
2022-09-05 00:50:16 +0300

    legacy code fix

--- cc.js
+++
@@ -1,7 +0,0 @@
-import * as curlconverter from "./dist/src/index.js";
-
-let toPython = curlconverter.toJavaJsoup(
-  'curl -X POST -H \'Content-Type: application/json\' -d \'{"username":"davidwalsh","password":"something"}\' http://domain.tld/login\n'
-);
-
-console.log(toPython);
