07d099aae23c3a6a70caadb2d5f2bedc7a8777a6
Boris Verkhovskiy <boris.verk@gmail.com>
2022-12-11 19:15:35 -0700

    Go: comment out header to keep auto-decompression

--- test/fixtures/go/get_with_browser_headers.go
+++ test/fixtures/go/get_with_browser_headers.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
+	// req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.8")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
