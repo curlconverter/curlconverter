62760661b2ac1a8943ea7d850636e7c0e4a46ca5
Boris Verkhovskiy <boris.verk@gmail.com>
2024-03-31 12:55:42 -0700

    Load tree-sitter-bash.wasm from JS directory

--- src/shell/webParser.ts
+++ src/shell/webParser.ts
@@ -5,7 +5,7 @@ import Parser from "web-tree-sitter";
 // NOTE: top-level await requires Safari 15+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await#browser_compatibility
 await Parser.init();
-const Bash = await Parser.Language.load("/tree-sitter-bash.wasm");
+const Bash = await Parser.Language.load("./tree-sitter-bash.wasm");
 const parser = new Parser();
 parser.setLanguage(Bash);
 
